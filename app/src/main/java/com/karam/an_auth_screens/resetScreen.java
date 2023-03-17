dedc1accc026d5e8e8edae59f14ded6a402c2cb4
karam-zomlut <zomlutk@gmail.com>
2023-03-17 23:20:42 +0200

    Finish Reset Password Screen

---
+++ app/src/main/java/com/karam/an_auth_screens/resetScreen.java
@@ -0,0 +1,14 @@
+package com.karam.an_auth_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resetScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_screen);
+    }
+}
